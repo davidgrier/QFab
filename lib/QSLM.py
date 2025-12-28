7ef3c2658c3481be6775c54df5f726e8248403cc
David Grier <david.grier@nyu.edu>
2025-12-28 17:58:53 -0500

    use faster API

--- lib/QSLM.py
+++ lib/QSLM.py
@@ -1,6 +1,6 @@
+from pyqtgraph import (GraphicsLayoutWidget, ImageItem)
 from pyqtgraph.Qt.QtCore import (Qt, pyqtSlot, pyqtProperty)
-from pyqtgraph.Qt.QtWidgets import QLabel
-from pyqtgraph.Qt.QtGui import (QImage, QPixmap, QGuiApplication)
+from pyqtgraph.Qt.QtGui import QGuiApplication
 import numpy as np
 import logging
 
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 
 
-class QSLM(QLabel):
+class QSLM(GraphicsLayoutWidget):
 
     '''Spatial Light Modulator interface
 
@@ -35,51 +35,48 @@ class QSLM(QLabel):
     Hologram = np.ndarray[np.uint8]
 
     def __init__(self, *args, fake: bool = False, **kwargs) -> None:
-        super().__init__(None)
+        super().__init__(*args, **kwargs, show=True)
+        self._setupUi()
+
+    def _setupUi(self) -> None:
+        self.ci.layout.setContentsMargins(0, 0, 0, 0)
+        self.view = self.addViewBox(enableMenu=False,
+                                    enableMouse=False)
+        self.view.setDefaultPadding(0)
+        self.image = ImageItem(axisOrder='row-major')
+        self.view.addItem(self.image)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+
         screens = QGuiApplication.screens()
         if (len(screens) == 2) and not fake:
             logger.debug('Opening SLM on secondary screen')
             screen = screens[1]
-            geometry = screen.availableGeometry()
-            self.setWindowFlags(Qt.FramelessWindowHint)
-            self.setGeometry(geometry)
-            self.show()
-            self.windowHandle().setScreen(screen)
-            self.showFullScreen()
+            geometry = screen.genometry()
+
+            self.showMaximized()
         else:
             x0, y0, w, h = 100, 100, 640, 480
             self.setGeometry(x0, y0, w, h)
             self.resize(w, h)
-            self.show()
-        phase = np.zeros((self.height(), self.width()), dtype=np.uint8)
-        self.data = phase
+        self.setData(np.zeros(self.shape, dtype=np.uint8))
 
     @pyqtProperty(tuple)
     def shape(self) -> tuple[int, int]:
         return (self.height(), self.width())
 
     @pyqtSlot(np.ndarray)
-    def setData(self, data: Hologram) -> None:
+    def setData(self, hologram: Hologram) -> None:
         logger.debug('Setting SLM data')
-        self.data = data
+        self.image.setImage(hologram, autoLevels=False)
 
-    @pyqtProperty(np.ndarray)
-    def data(self):
-        return self._data
-
-    @data.setter
-    def data(self, d: Hologram) -> None:
-        self._data = d
-        self.qimage = QImage(d.data,
-                             d.shape[1], d.shape[0], d.strides[0],
-                             QImage.Format.Format_Indexed8)
-        self.setPixmap(QPixmap.fromImage(self.qimage))
+    def data(self) -> Hologram:
+        return self.image.data
 
 
 def example():
-    from pyqtgraph import mkQApp
+    import pyqtgraph as pg
 
-    app = mkQApp()
+    app = pg.mkQApp()
     slm = QSLM()
     phase = np.indices(slm.shape).sum(axis=0) % 256
     slm.setData(phase.astype(np.uint8))
